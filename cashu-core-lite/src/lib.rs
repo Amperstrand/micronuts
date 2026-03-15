99225cbe7002305f79b2fb47e8aafdab51349736
mayor <amperstrand@localhost>
2026-03-15 18:41:54 +0100

    Merge remote-tracking branch 'origin/polecat/slit/mn-r1e@mmrxthfu'

--- cashu-core-lite/src/lib.rs
+++ cashu-core-lite/src/lib.rs
@@ -6,5 +6,7 @@ extern crate alloc;
 pub mod crypto;
 pub mod token;
 
-pub use crypto::{blind_message, hash_to_curve, unblind_signature, BlindedMessage, HashToCurveError};
+pub use crypto::{
+    blind_message, hash_to_curve, unblind_signature, BlindedMessage, HashToCurveError,
+};
 pub use token::{decode_token, encode_token, Proof, TokenV4, TokenV4Token};
